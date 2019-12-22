10ad46ee56f1588d32a957f2536d63659a8a312f
Saneklab <58747163+Saneklab@users.noreply.github.com>
2019-12-22 22:16:40 +0300

    pyt.py

---
+++ lyb1/pyt.py
@@ -0,0 +1,10 @@
+from math import *
+x=float(input('Введите x:'))
+a=float(input('Введите a:'))
+G=(-5*(a**2+a*x-2*x**2))/(20*a**2+41*a*x+20*x**2)
+F=cos(a**2+2*a*x-3*x**2)
+Y=acos(-45*a**2-8*a*x+21*x**2+1)
+print (G)
+print (F)
+print (Y)
+
